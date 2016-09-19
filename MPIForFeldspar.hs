90d60cfa53389d8c86a6786307ab23f86f487c6e
Maximilian Algehed <m.algehed@gmail.com>
2016-09-19 10:45:58 +0200

    Added group communication to the MPI interface

--- MPIForFeldspar.hs
+++ MPIForFeldspar.hs
@@ -10,34 +10,35 @@ import qualified Prelude as P
 import Feldspar.Data.Vector
 import Feldspar.Run
 
-type Communicator = FunArg Data PrimType'
+data Communicator = C (FunArg Data PrimType')
+data Group        = G (FunArg Data PrimType') 
 type Node         = Data Word32
 
 root :: Node
 root = 0
 
 getRank :: Communicator -> Run (Data Word32)
-getRank comm = do
+getRank (C comm) = do
             rank_ref <- initRef (0 :: Data Word32)
             callProc "MPI_Comm_rank" [comm, refArg rank_ref]
             getRef rank_ref
 
 getSize :: Communicator -> Run (Data Word32)
-getSize comm = do
+getSize (C comm) = do
             size_ref <- initRef (0 :: Data Word32)
             callProc "MPI_Comm_size" [comm, refArg size_ref]
             getRef size_ref
 
 -- | Default world communicator
-mpi_comm_world :: Communicator
-mpi_comm_world = constArg "" "MPI_COMM_WORLD"
+worldComm :: Communicator
+worldComm = C $ constArg "" "MPI_COMM_WORLD"
 
 -- | Set up MPI
 setup = do
           addInclude "<mpi.h>"
           callProc "MPI_Init" [valArg (0 :: Data Word32), valArg (0 :: Data Word32)]
-          rank <- getRank mpi_comm_world
-          size <- getSize mpi_comm_world
+          rank <- getRank worldComm
+          size <- getSize worldComm
           return (rank, size)
 
 -- | finalize mpi
@@ -45,7 +46,7 @@ finish = callProc "MPI_Finalize" []
 
 -- | Run a computation on some group of nodes
 on :: (DManifestable a Word32) => a -> Communicator -> Run ()-> Run ()
-on as comm r = do
+on as (C comm) r = do
           rank <- getRank comm
           locations <- manifested as
           iff (fold (||) false $ map (rank==) locations)
@@ -54,7 +55,7 @@ on as comm r = do
 
 -- | Run on all nodes but some group of nodes
 onAllBut :: (DManifestable a Word32) => a -> Communicator -> Run ()-> Run ()
-onAllBut as comm r = do
+onAllBut as (C comm) r = do
           rank <- getRank comm
           locations <- manifested as
           iff (not (fold (||) false $ map (rank==) locations))
@@ -68,7 +69,7 @@ send :: (MPITypeable a, MPIReferable a, Sizeable a)
     -> Data Word32   -- Tag
     -> Communicator  -- What Communicator to use
     -> Run ()
-send a target tag comm =
+send a target tag (C comm) =
     do
       r <- refer a
       callProc "MPI_Send" [r,
@@ -85,7 +86,7 @@ recv :: forall a. (Syntactic a, MPITypeable a, Syntax a, PrimType' (Internal a),
     -> Data Word32  -- Tag
     -> Communicator -- The communicator to use
     -> Run (Arr a)
-recv n target tag comm =
+recv n target tag (C comm) =
     do
         arr <- newArr n 
         callProc "MPI_Recv" [arrArg arr,
@@ -104,7 +105,7 @@ bcast :: (PrimType' (Internal a), MPITypeable a, Sizeable a, Syntax a)
       -> Node
       -> Communicator
       -> Run ()
-bcast arr n node comm = 
+bcast arr n node (C comm) = 
     do
         a <- getArr arr 0
         callProc "MPI_Bcast" [arrArg arr,
@@ -119,7 +120,7 @@ gatherSend :: forall a. (PrimType' (Internal a), MPITypeable a, Sizeable (Arr a)
            -> Node
            -> Communicator
            -> Run ()
-gatherSend arr node comm =
+gatherSend arr node (C comm) =
     do
         callProc "MPI_Gather" [arrArg arr,
                                valArg (size arr),
@@ -136,7 +137,7 @@ gatherRecv :: forall a. (PrimType' (Internal a), MPITypeable a, Sizeable (Arr a)
            -> Arr a
            -> Communicator
            -> Run ()
-gatherRecv arr arrS comm =
+gatherRecv arr arrS (C comm) =
     do
         rank <- getRank comm
         callProc "MPI_Gather" [arrArg arrS,
@@ -153,7 +154,7 @@ commSplit :: Data Word32  -- Colour
           -> Data Word32  -- Key
           -> Communicator -- Communicator to split
           -> Run Communicator
-commSplit colour key comm = do
+commSplit colour key (C comm) = do
     comm' <- newObject "MPI_Comm" False
     let arg = objArg comm'
     callProc "MPI_Comm_split" [comm, valArg colour, valArg key, addr arg]
@@ -161,4 +162,29 @@ commSplit colour key comm = do
 
 -- | Freeing a communicator
 commFree :: Communicator -> Run ()
-commFree comm = callProc "mpi_Comm_free" [addr comm]
+commFree (C comm)  = callProc "mpi_Comm_free" [addr comm]
+
+-- | Get the group from a communicator
+commGroup :: Communicator -> Run Group
+commGroup (C comm) = do
+    g <- newObject "MPI_Group" False
+    callProc "mpi_Comm_group" [comm, addr (objArg g)]
+    return $ G (objArg g)
+
+-- | Take the union of two groups
+groupUnion :: Group -> Group -> Run Group
+groupUnion (G g1) (G g2) = do
+    g <- newObject "MPI_Group" False
+    callProc "mpi_Group_union" [g1, g2, addr g]
+    return $ G g
+
+-- | Take the intersection of two groups
+groupIntersection :: Group -> Group -> Run Group
+groupIntersection (G g1) (G g2) = do
+    g <- newObject "MPI_Group" False
+    callProc "mpi_Group_intersection" [g1, g2, addr g]
+    return $ G g
+
+-- | Free a group
+groupFree :: Group -> Run ()
+groupFree (G g) = callProc "mpi_Group_free" [addr g]
