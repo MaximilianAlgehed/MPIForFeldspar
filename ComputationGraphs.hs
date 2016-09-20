70052cf03e9a15edc4702b204e609c2c061e6d04
Maximilian Algehed <m.algehed@gmail.com>
2016-09-20 22:52:44 +0200

    I have 000000 idea what I'm doing

--- ComputationGraphs.hs
+++ ComputationGraphs.hs
@@ -1,2 +1,12 @@
 module ComputationGraphs where
+import Feldspar.Run
+import Data.Graph.Inductive as G hiding (Node, Edge)
+import qualified MPIForFeldspar as MPI
 
+data Node a = Node {computation :: a -> Run a,
+                    allocation  :: Maybe MPI.Node 
+                   }
+
+data Edge a = Edge {size :: Data Length}
+
+data CGraph a = CGraph {graph :: Gr (Node a) (Edge a)}
--- MPIFrontend.hs
+++ MPIFrontend.hs
@@ -2,13 +2,37 @@
              FlexibleInstances,
              FlexibleContexts,
              UndecidableInstances,
-             ScopedTypeVariables #-}
-module MPIForFeldspar where
+             ScopedTypeVariables,
+             GADTs,
+             TypeOperators #-}
+module MPIFrontend where
+import Language.Syntactic.Syntax hiding ((:+:))
+import Language.Embedded.Imperative
+import Language.Syntactic.Decoration
 import MPIForFeldspar
 import Feldspar.Representation
 import Typeclasses
 import qualified Prelude as P
 import Feldspar.Data.Vector
 import Feldspar.Run
+import Feldspar.Run.Representation
 
-data MPICMD = RunCMD :+: GroupCMD :+: CommCMD
+type MPICMD = RunCMD :+: CommCMD
+
+data CommCMD fs a where
+    On :: (DManifestable a Word32) => exp a -> exp Communicator -> prog () -> CommCMD (Param3 prog exp pred) ()
+
+data GroupConstructs sig where
+    Union        :: GroupConstructs (Group :-> (Group :-> Full Group))
+    Intersection :: GroupConstructs (Group :-> (Group :-> Full Group))
+
+type MPIDomain = (FeldConstructs :+: GroupConstructs) :&: TypeRepFun
+
+newtype MPIData a = MPIData {unMPIData :: ASTF MPIDomain a}
+
+newtype MPIRun a =
+    MPIRun {unRun :: ProgramT
+                        MPICMD
+                        (Param2 MPIData PrimType')
+                        (Program CompCMD (Param2 MPIData PrimType'))
+           }
